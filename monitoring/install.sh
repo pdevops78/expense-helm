18122329831d3192d4cf5604c49d1d991d9401b2
Your Name <vemasani.pavani@gmail.com>
2025-09-11 05:23:33 +0530

    backend

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -1,2 +1,2 @@
 helm preometheus repo: https://prometheus-community.github.io/helm-charts
-helm install pstack oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack
+helm install pstack oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack -f pstack-values.yaml
